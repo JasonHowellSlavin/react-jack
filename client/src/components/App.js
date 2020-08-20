a37d08444a311c8d7714deafed72305227ad193c
Jason Slavin <jslavin@marketo.com>
2020-08-20 13:38:15 -0700

    Finishing basic Redux Tutorial, going to build off of to ensure everything sticks

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.scss';
+import Counter  from './Counter/Counter';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const initialState = {
+    count: 0
+  };
+
+function reducer(state = initialState, action) {
+    console.log('reducer', state, action);
+    switch(action.type) {
+        case "INCREMENT":
+            return {
+                count: state.count + 1
+            }
+        case "DECREMENT":
+            return {
+                count: state.count - 1
+            }
+        case "RESET":
+            return {
+                count: 0
+            }
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
+store.dispatch({ type: "RESET" });
 
 class App extends React.Component{
     render(){
         return(
-            <h1>Hello World!!</h1>
+            <Provider store={store}>
+                <Counter />
+            </Provider>
         )
     }
 }
